6fd0aa466b009da6b02dd3e8c6f48db68c328ff6
OSAMAJOTA <jotaosama@gmail.com>
2024-04-29 16:33:29 +0300

    agents details

--- resources/views/agents/agents_detils.blade.php
+++ resources/views/agents/agents_detils.blade.php
@@ -15,8 +15,8 @@
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">   العملاء</h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/
-                     بيانات العميل</span>
+                <h4 class="content-title mb-0 my-auto"><a href="/agents">العملاء</a>   </h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/
+                     بيانات العميل/ <b>{{$agents->agents_name}}</b></span>
             </div>
         </div>
 
